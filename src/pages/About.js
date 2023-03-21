f7428b5f4c4d4351ceb43f79929aec596f6abb32
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2023-03-21 15:07:37 +0100

    Cocktail db

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus id nisi vel iaculis. Nunc eu dolor eu nibh mattis molestie sit amet at ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin lobortis justo in nisl euismod tempus. Sed elementum, lorem eu ornare varius, elit justo fermentum ipsum, id faucibus ex nisi id felis. Ut a aliquet sapien. Nullam id elit porttitor, semper tellus eget, suscipit urna. Nullam iaculis eleifend velit eget volutpat. Aenean vel rhoncus lectus. Proin vel dignissim urna, sit amet condimentum lorem. Phasellus ut diam in ipsum pretium convallis. Ut.
+      </p>
+    </section>
+  )
+}
+
+export default About
